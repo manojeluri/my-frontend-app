1a13d60954e6a3959b3a7ef20c5ce1034cdc3ce3
Manoj Eluri <manojeluri@Manojs-MacBook-Pro.local>
2024-10-21 03:54:05 +0530

    Signup functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+// App.js
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login';
 
@@ -11,13 +13,11 @@ function App() {
         <nav>
           <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link>
         </nav>
-        <Switch>
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/">
-            <h2>Welcome to the App</h2>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<h2>Welcome to the App</h2>} />
+        </Routes>
       </div>
     </Router>
   );
