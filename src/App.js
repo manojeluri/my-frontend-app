cb478e8f416d900a344b1ab1805f77e46cfba4ba
Manoj Eluri <manojeluri@Manojs-MacBook-Pro.local>
2024-10-21 02:01:53 +0530

    Initial commit of React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('https://enigmatic-hollows-82185-701449e24cf2.herokuapp.com/')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1>React Frontend</h1>
+      <p>Message from Backend:</p>
+      <h2>{message}</h2>
     </div>
   );
 }
